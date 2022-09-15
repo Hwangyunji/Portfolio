742dcf548e1401ff80282c6ec2e6b1cdfe541dad
Hwangyunji <105402299+Hwangyunji@users.noreply.github.com>
2022-09-15 11:12:38 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
   $('#fullpage').fullpage({
     //options here
     // responsiveWidth: 1000, // fullpage 해제 분기점
-    // sectionsColor: ['gold', '#1abc9c', '#C0C0C0', 'pink'],
     anchors: ['HOME', 'PROFILE', 'WORK', 'CONTACT'],
     menu: '#enu',
     navigation: true,
@@ -13,9 +12,7 @@ $(document).ready(function () {
 
   // 메인에서 상단이동
   $('#btnGoTop').click(function () {
-    //$.fn.fullpage.setScrollingSpeed(0); 효과를 없애고싶을때
     $.fn.fullpage.moveTo(1, 1); // 이동하고싶은 페이지
-    //$.fn.fullpage.setScrollingSpeed(700); 효과를 없애고싶을때
   });
 
 
